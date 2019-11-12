f35d0fd62e2ec76818567aad63765201e0e1f087
JDR <zhoude@vcrecit.com>
2019-11-12 15:59:15 +0800

    delay-message: 1、init commit

---
+++ delay-message/src/main/java/com/jdr/maven/rabbitmq/delay/delaymessage/DelayMessageApplication.java
@@ -0,0 +1,13 @@
+package com.jdr.maven.rabbitmq.delay.delaymessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelayMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelayMessageApplication.class, args);
+    }
+
+}
---
+++ delay-message/src/test/java/com/jdr/maven/rabbitmq/delay/delaymessage/DelayMessageApplicationTests.java
@@ -0,0 +1,17 @@
+package com.jdr.maven.rabbitmq.delay.delaymessage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DelayMessageApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
